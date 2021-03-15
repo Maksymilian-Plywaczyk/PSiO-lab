bec105aab1f77c44feda17805d18688f3cacda04
Maksymilian Pływaczyk <plywak12331@gmail.com>
2021-03-15 18:41:44 +0100

    Add exercise 1, function is_palindrome

--- PSiO-lab2/PSiO-lab2.cpp
+++ PSiO-lab2/PSiO-lab2.cpp
@@ -1,8 +1,30 @@
 ﻿#include <iostream>
+#include <string>
 
+
+
+bool is_palindrome(std::string& word)
+{
+    for(int i=0; i<word.length();i++)
+    {
+        if (word[i] != word[word.length()-i-1])
+        {
+            return false;
+        }
+    }
+    return true;
+}
 int main()
 {
-    std::cout << "Hello World!\n";
-    std::cout << "it works";
+    std::string word = "racecar";
+    if (is_palindrome(word)) {
+        std::cout << "To palindrom!" << std::endl;
+    }
+    else {
+        std::cout << "Nope" << std::endl;
+    }
+
+
+    
 }
 
