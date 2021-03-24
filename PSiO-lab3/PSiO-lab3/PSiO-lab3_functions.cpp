a4ba4a54445f78e4323968e377cfcd1b01a50f04
Maksymilian Pływaczyk <plywak12331@gmail.com>
2021-03-24 22:24:47 +0100

    add exercise1

---
+++ PSiO-lab3/PSiO-lab3/PSiO-lab3_functions.cpp
@@ -0,0 +1,34 @@
+#include "PSiO-lab3_functions.h"
+
+
+int randomInt(int min, int max)
+{
+	static std::default_random_engine e{ static_cast<long unsigned int>(time(0)) };
+	std::uniform_int_distribution<int> d{ min, max };
+	return d(e);
+}
+void exercise1(const int& range,  int erase_value)
+{
+	std::vector<int>numbers;
+	for (int i = 0; i < range; i++)
+	{
+		numbers.push_back(randomInt(-20, 20));
+		
+	}
+	
+	for (std::vector<int>::iterator it = numbers.begin(); it != numbers.end(); it++)
+	{
+		if (*it == erase_value)
+		{
+			it= numbers.erase(it);
+		}
+		it++;
+	}
+	for (std::vector<int>::iterator it = numbers.begin(); it != numbers.end(); it++)
+	{
+		
+		std::cout << *it << std::endl;
+	}
+
+
+}
--- PSiO-lab3/PSiO-lab3/PSiO-lab3_functions.h
+++ PSiO-lab3/PSiO-lab3/PSiO-lab3_functions.h
@@ -1,1 +1,9 @@
 #pragma once
+#include <iostream>
+#include <vector>
+#include <random>
+#include <ctime>
+
+int randomInt(int min, int max);
+void exercise1(const int& range,   int erase_value);
+
