83065e60b8fdab04bedcc2ec73ba8cd4a570e762
Maksymilian Pływaczyk <plywak12331@gmail.com>
2021-04-09 19:37:59 +0200

    add simply collision

--- PSiO-lab_SFML/PSiO-lab_SFML/PSiO-lab_SFML.cpp
+++ PSiO-lab_SFML/PSiO-lab_SFML/PSiO-lab_SFML.cpp
@@ -1,30 +1,28 @@
 ﻿#include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 #include <iostream>
-#include <time.h>
+
 
 int main() {
     // create the window
-    const float WindowLenght = 800;
-    const float WindowWidth = 600;
-    sf::RenderWindow window(sf::VideoMode(WindowLenght, WindowWidth), "My window");
+     float WindowWidth = 800;
+     float WindowHeight = 600;
+    sf::RenderWindow window(sf::VideoMode(WindowWidth, WindowHeight), "My window");
     
     // create some shapes
     sf::CircleShape circle(100.0f);
     circle.setPosition(100.0f, 300.0f);
     circle.setFillColor(sf::Color(100, 250, 50));
 
-    
-    const float rectangle_velocity_x = 10.f;
-    const float rectangle_velocity_y = 15.f;
-    const float rectangle_angular_velocity = 0.01f;
+  
     sf::RectangleShape rectangle(sf::Vector2f(120.0, 60.0));
-    rectangle.setPosition(500.0, 400.0);
+    rectangle.setPosition(200.0, 400.0);
     rectangle.setFillColor(sf::Color(100, 50, 250));
     //rectangle.move(rectangle_velocity_x,rectangle_velocity_y);
 
-
-    
+    float rectangle_velocity_x = 50;
+    float rectangle_velocity_y = 150;
+    float rectangle_angular_velocity = 10;
     sf::ConvexShape triangle;
     triangle.setPointCount(3);
     triangle.setPoint(0, sf::Vector2f(0.0, 0.0));
@@ -37,63 +35,52 @@ int main() {
 
     sf::Clock clock;
 
+    //window.setFramerateLimit(60);
+
     // run the program as long as the window is open
     while (window.isOpen()) {
-        
-        sf::Time elapsed = clock.getElapsedTime();
-        float lastTime = 0;
-       
+  
+      
         // check all the window's events that were triggered since the last iteration of the loop
         sf::Event event;
         while (window.pollEvent(event)) {
             // "close requested" event: we close the window
             if (event.type == sf::Event::Closed)
                 window.close();
-            if (event.type == sf::Event::KeyReleased);
-                //elapsed = clock.restart();
         }
-        
-        float currentTime = clock.restart().asMicroseconds();
-       /* float fps = 1.f / (currentTime - lastTime);
-        lastTime = currentTime;*/
-        
-        sf::Vector2f speed(rectangle_velocity_x, rectangle_velocity_y);//velocity of rectangle
-        float distance_x = speed.x / currentTime;//distance= speed/time;
-        float distance_y = speed.y / currentTime;
-        
-        rectangle.move(distance_x,distance_y);
-        rectangle.rotate(rectangle_angular_velocity);
+        //LOGIKA
+
+        sf::Time currentTime = clock.restart();
+  
        
-        sf::FloatRect rectangle_bounds = rectangle.getGlobalBounds();
-        if (rectangle.getPosition().x <= 0.f)
-        {
-            rectangle.move(distance_x, distance_y);
-
-            rectangle.rotate(rectangle_angular_velocity);
-            std::cout << "collision";
-        }
-       if (rectangle.getPosition().y <= 0.f)
-        {
-            rectangle.move(distance_x,distance_y);
-
-            rectangle.rotate(rectangle_angular_velocity);
-            //std::cout << "collision";
-        }
-       if (rectangle.getPosition().x + rectangle_bounds.width >= window.getSize().x);
-        {
-            rectangle.move(distance_x, distance_y);
-
-            rectangle.rotate(rectangle_angular_velocity);
-            std::cout << "collision" << std::endl;
-        }
-        if (rectangle.getPosition().y + rectangle_bounds.height >= window.getSize().y);
-        {
-            rectangle.move(distance_x, distance_y);
-
-            rectangle.rotate(rectangle_angular_velocity);
-            std::cout << "collision";
-        }
+        rectangle.move(rectangle_velocity_x * currentTime.asSeconds(), rectangle_velocity_y * currentTime.asSeconds());
+        rectangle.rotate(rectangle_angular_velocity*currentTime.asSeconds());
         
+        
+        sf::FloatRect rectangle_bounds = rectangle.getGlobalBounds();
+        std::cout << rectangle_bounds.top << " " << rectangle_bounds.left << " ";
+        std::cout << rectangle_bounds.width << " " << rectangle_bounds.height << std::endl;
+        if (rectangle_bounds.top <= 0)
+        {
+            rectangle_velocity_y = -rectangle_velocity_y;
+        }
+        if (rectangle_bounds.top + rectangle_bounds.height >= window.getSize().y)
+        {
+            
+            rectangle_velocity_y -= rectangle_velocity_y;
+        }
+        if (rectangle_bounds.left <= 0)
+        {
+            rectangle_velocity_x = -rectangle_velocity_x;
+
+        }
+       if (rectangle_bounds.left + rectangle_bounds.width >= window.getSize().x)
+        {
+           rectangle_velocity_x =  -rectangle_velocity_x;
+          
+        }
+    
+       
         // clear the window with black color
         window.clear(sf::Color::Black);
 
