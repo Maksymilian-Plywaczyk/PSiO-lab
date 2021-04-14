e36c63adc16e4d9979636d9f4af7a74de5e74fce
Maksymilian Pływaczyk <plywak12331@gmail.com>
2021-04-14 23:54:13 +0200

    Inheritance sfml learing

--- PSiO-lab_SFML2/PSiO-lab_SFML2/PSiO-lab_SFML2.cpp
+++ PSiO-lab_SFML2/PSiO-lab_SFML2/PSiO-lab_SFML2.cpp
@@ -40,14 +40,34 @@ private:
 class Traktor : public Vehicle
 {
 public:
-    Traktor();
+   
+    Traktor(const std::string& name,bool color) :Vehicle(name, 4, "Diesel", 100),color(color) {}
+    bool has_color() { return color; }
+private:
+   bool color;
+};
+
+class CustomRectangleShape:public sf::RectangleShape
+{
+public:
+    sf::Vector2f size;
+    sf::Vector2f position;
+    CustomRectangleShape(const sf::Vector2f& size_, const sf::Vector2f& position_) :size(size_), position(position_) {}
+    sf::Vector2f sizeS() { return size; }
 };
 
 int main()
 {
+    sf::Vector2f size(120.0, 60.0);
+    sf::Vector2f position(120.0, 60.0);
+    CustomRectangleShape my_rectangle(size, position);
+    my_rectangle.setFillColor(sf::Color(100, 50, 100));
+    std::cout << my_rectangle.sizeS().x << std::endl;
+    Traktor traktor("Wielki ciagnik",false);
     Car car("Toyota","Gasoline",200.0,false);
     std::cout << car.name()<<" "<<car.propulsion_type() << std::endl;
-
+    std::cout << traktor.has_color() << std::endl;
+  
 
    
     std::cout << "Hello World!\n";
